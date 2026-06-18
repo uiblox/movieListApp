ba4da53e90cd547888f5a2a224862ebc345fd428
Christian <bournecreative@gmail.com>
2026-06-18 00:02:15 -0500

    Create MoveList temporary card.

--- src/pages/MovieList.tsx
+++ src/pages/MovieList.tsx
@@ -1,7 +1,7 @@
 export const MovieList = () => {
   return (
     <main>
-      <h1 className="text-3xl font-bold underline">Movie List</h1>
+      <div className="">Movie List</div>
     </main>
   );
 };
