1a581f8a27ec6e0b75861366efdbcf639c6db0b5
Christian <bournecreative@gmail.com>
2026-06-24 01:43:16 -0500

    MovieCard: Create movie card UI to display an image, title, movie description and link to navigate to movie.

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,7 +1,22 @@
-export const MovieCard = () => {
+import { Link } from "react-router";
+
+export const MovieCard = ({ movieData }: { movieData: any }) => {
+  console.log("what is movie data", movieData.title);
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Movie Card</h1>
+    <div className="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs dark:border-gray-100">
+      <Link to={`/movie/${movieData.id}`}>
+        <img
+          className="rounded-base"
+          src={`https://image.tmdb.org/t/p/w500/${movieData.poster_path}`}
+          alt=""
+        />
+      </Link>
+      <Link to={`/movie/${movieData.id}`}>
+        <h5 className="mt-6 mb-2 text-2xl font-semibold tracking-tight text-heading dark:text-white">
+          {movieData.title}
+        </h5>
+      </Link>
+      <p className="mb-6 text-body dark:text-white">{movieData.overview}</p>
     </div>
   );
 };
