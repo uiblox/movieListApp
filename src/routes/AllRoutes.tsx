b1524080d9d7459d860e65e2ea45e21fbcfd5ada
Christian <bournecreative@gmail.com>
2026-06-25 00:10:53 -0500

    useFetchHook: For specific routes specify unique target path to pass to the page/component that will fetch content based on url target.

--- src/routes/AllRoutes.tsx
+++ src/routes/AllRoutes.tsx
@@ -5,12 +5,24 @@ export const AllRoutes = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<MovieList />} />
+        <Route
+          path="/"
+          element={<MovieList apiTargetPath="movie/now_playing" />}
+        />
         <Route path="/movie/:id" element={<MovieItem />} />
-        <Route path="/movies/popular" element={<MovieList />} />
-        <Route path="/movies/top" element={<MovieList />} />
-        <Route path="/movies/upcoming" element={<MovieList />} />
-        <Route path="/search" element={<Search />} />
+        <Route
+          path="/movies/popular"
+          element={<MovieList apiTargetPath="movie/popular" />}
+        />
+        <Route
+          path="/movies/top"
+          element={<MovieList apiTargetPath="movie/top_rated" />}
+        />
+        <Route
+          path="/movies/upcoming"
+          element={<MovieList apiTargetPath="movie/upcoming" />}
+        />
+        <Route path="search" element={<Search />} />
         <Route path="*" element={<NotFound404 />} />
       </Routes>
     </>
