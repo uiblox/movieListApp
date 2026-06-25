67f435e3a49bcaa3ad693866d34b496a8ac51511
Christian <bournecreative@gmail.com>
2026-06-25 00:16:27 -0500

    useFetchHook: Implement useFetchHook to consume now playing movie end point.

--- src/pages/MovieList.tsx
+++ src/pages/MovieList.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
 import { MovieCard } from "../components";
+import { useFetch } from "../hooks/useHooks";
 
 interface MovieType {
   adult: boolean;
@@ -11,30 +11,23 @@ interface MovieType {
   original_title: string;
 }
 
-export const MovieList = () => {
-  const [movies, setMovies] = useState<MovieType[]>([]);
+interface MovieListProps {
+  apiTargetPath: string;
+}
 
-  const movie_key = import.meta.env.VITE_API_KEY;
-  useEffect(() => {
-    async function fetchMovies() {
-      const fetchResponse = await fetch(
-        `https://api.themoviedb.org/3/movie/now_playing?api_key=${movie_key}`,
-      );
-      const data = await fetchResponse.json();
-      console.log(data);
-      setMovies(data.results);
-    }
-    fetchMovies();
-  }, []);
+export const MovieList: React.FC<MovieListProps> = ({
+  apiTargetPath,
+}: MovieListProps) => {
+  const { data: movies } = useFetch(apiTargetPath);
+
   return (
     <main>
       <section className="max-w-7xl mx-auto py-7">
         <div className="flex justify-center lg:justify-start flex-wrap gap-4">
           {movies.length
-            ? movies.map((movie) => {
-                console.log("this is the moveie", movie.title);
-                return <MovieCard key={movie.id} movieData={movie} />;
-              })
+            ? movies.map((movie) => (
+                <MovieCard key={movie.id} movieData={movie} />
+              ))
             : "no movies"}
         </div>
       </section>
