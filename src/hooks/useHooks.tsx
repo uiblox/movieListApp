5733fad17f58e07732dd642be809e1b8151167eb
Christian <bournecreative@gmail.com>
2026-06-25 00:14:19 -0500

    useFetchHook: Migrate useFetch logic into useFetch hook. Consume apiTargetPath to generate dynamic end points to fetch.

---
+++ src/hooks/useHooks.tsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+export const useFetch = (apiTargetPath: string) => {
+  const [data, setData] = useState([]);
+  const movie_key = import.meta.env.VITE_API_KEY;
+  const url = `https://api.themoviedb.org/3/${apiTargetPath}?api_key=${movie_key}`;
+
+  useEffect(() => {
+    async function fetchMovies() {
+      const fetchResponse = await fetch(url);
+      const jsonResponse = await fetchResponse.json();
+      setData(jsonResponse.results);
+    }
+    fetchMovies();
+  }, [url]);
+
+  return { data };
+};
