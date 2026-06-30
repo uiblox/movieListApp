de128b543cbb60421eeb35abf81c444bf15673a1
Christian <bournecreative@gmail.com>
2026-06-30 01:32:18 -0500

    ScrollToTop: Create a utility that ensure the page scroll is reset to the top of the window when a navigate event occurs.

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
+import ScrollToTop from "./utils/ScrollToTop.ts";
+
 import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </StrictMode>,
