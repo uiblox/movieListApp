96e1b7704188be219e3dd244ed415566fddb7c47
Christian <bournecreative@gmail.com>
2026-07-01 22:47:23 -0500

    useFetch - add an interface for Movie to include id, poster_path, title and overview.

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,15 +1,26 @@
 import { useState, useEffect } from "react";
 
-export const useFetch = (apiTargetPath: string, queryTerms: string = "") => {
-  const [data, setData] = useState([]);
+export interface Movie {
+  id: number;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+
+export const useFetch = <T,>(
+  apiTargetPath: string,
+  queryTerms: string | null = "",
+) => {
+  const [data, setData] = useState<T[]>([]);
   const movie_key = import.meta.env.VITE_API_KEY;
-  const url = `https://api.themoviedb.org/3/${apiTargetPath}?api_key=${movie_key}&query=${queryTerms}`;
+  const normalizedQuery = queryTerms ?? "";
+  const url = `https://api.themoviedb.org/3/${apiTargetPath}?api_key=${movie_key}&query=${normalizedQuery}`;
 
   useEffect(() => {
     async function fetchMovies() {
       const fetchResponse = await fetch(url);
       const jsonResponse = await fetchResponse.json();
-      setData(jsonResponse.results);
+      setData((jsonResponse.results ?? []) as T[]);
     }
     fetchMovies();
   }, [url]);
